473a5637137fb347af81c623973f23aecb8022ed
suleiman-moraes <suleimanmoraes@gmail.com>
2025-12-24 12:15:02 +0000

    Configure eureka server

---
+++ src/test/java/com/moraes/arq_base/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moraes.arq_base.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
